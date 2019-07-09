2679691317437cca39d55ebc32621fd8ca37c637
Minghao <liuminghao0830@gmail.com>
2019-07-09 16:36:42 -0700

    string 522 LCS

---
+++ 522_Longest_Uncommon_Subsequence_II.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+public:
+    bool LCS(string a, string b){
+        if (b.size() < a.size()) return false;
+        int i = 0;
+        for (char c : b){
+            if (a[i] == c) i++;
+            if (i == a.size()) break;
+        }
+        return i == a.size();
+    }
+    
+    int findLUSlength(vector<string>& strs){
+        if (strs.empty()) return -1;
+        int res = -1;
+
+        for (int i = 0; i < strs.size(); ++i){
+            int j;
+            for (j = 0; j < strs.size(); ++j){
+                if (i == j) continue;
+                if (LCS(strs[i], strs[j])) break;
+            }
+            if (j == (int)strs.size()) res = max(res, (int)strs.size());
+        }
+
+        return res;
+    }   
+};
+
+int main(){
+    Solution* solution = new Solution();
+    vector<string> strs{"aba", "cdc", "eae"};
+
+    cout << solution->findLUSlength(strs) << endl;
+    
+    return 0;
+}
