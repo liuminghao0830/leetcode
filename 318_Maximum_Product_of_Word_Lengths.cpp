16a81da26777bde0c570916e3e52657c34a53650
Minghao <liuminghao0830@gmail.com>
2019-07-28 05:24:40 -0700

    bit 318

---
+++ 318_Maximum_Product_of_Word_Lengths.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+public:
+    int maxProduct(vector<string> words){
+        unordered_map<string, int> m;
+        for (string w : words){
+            for (char c : w){
+                m[w] |= 1 << (c - 'a');
+            }
+        }
+
+        int max_len = 0;
+        for (int i = 0; i < words.size() - 1; ++i){
+            for (int j = i + 1; j < words.size(); ++j){
+                if (!(m[words[i]] & m[words[j]])){
+                    max_len = max(max_len, (int)words[i].length()*(int)words[j].length());
+                }
+            }
+        }
+
+        return max_len;
+    }
+};
+
+int main(){
+    vector<string> words{"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+    Solution* solution = new Solution();
+    cout << solution->maxProduct(words) << endl;
+    return 0;
+}
