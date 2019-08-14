9d80a0792dcae4aea27620e7d62d10f9cf565bbc
Minghao <liuminghao0830@gmail.com>
2019-08-14 02:00:20 -0700

    524 harsh table

--- 524_Longest_Word_in_Dictionary_through_Deleting.cpp
+++ 524_Longest_Word_in_Dictionary_through_Deleting.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     string findLongestWord(string s, vector<string>& d) {
+        /* sort O(nlogn) : 10% */
         sort(d.begin(), d.end(), [](string a, string b){
             if (a.size() == b.size()) return a < b;
             return a.size() > b.size();
@@ -19,6 +20,25 @@ public:
         }
         
         return "";
+    }
+
+    string findLongestWord_harsh_table(string s, vector<string>& d) {
+        /* harsh table O(n) : 67% */
+        unordered_map<int, string> m;
+        int max_len = 0;
+        
+        for (auto w : d){
+            int i = 0;
+            for (char c : s){
+                if (c == w[i]) i++;
+            }
+            if (i == w.length()){
+                if (!m.count(i) || w < m[i]) m[i] = w;
+                max_len = max(max_len, i);
+            }
+        }
+        
+        return max_len == 0 ? "" : m[max_len];
     }
 };
 
