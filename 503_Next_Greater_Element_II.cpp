381617260dc424649227c202b3b3bd005ba2d86a
Minghao <liuminghao0830@gmail.com>
2019-08-16 00:59:57 -0700

    stack 503

--- 503_Next_Greater_Element_II.cpp
+++ 503_Next_Greater_Element_II.cpp
@@ -31,6 +31,20 @@ public:
 
         return res;
     }
+
+    vector<int> nextGreaterElements_stack(vector<int>& nums) {
+        int n = nums.size(); vector<int> res(n, -1);
+        stack<int> stk;
+        for (int i = 0; i < 2 * n; ++i){
+            int num = nums[i % n];
+            while (!stk.empty() && nums[stk.top()] > num){
+                res[stk.top()] = num; stk.pop();
+            }
+            if (i < n) stk.push(i);
+        }
+
+        return res;
+    }
 };
 
 int main(){
